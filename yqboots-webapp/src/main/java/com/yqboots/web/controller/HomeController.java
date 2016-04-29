63b7e3d6fde915590f550a72e04eff55a8c40120
Eric H B Zhan <eric.zhan@163.com>
2016-04-29 08:03:21 +0800

    add new webapp module

---
+++ yqboots-webapp/src/main/java/com/yqboots/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.yqboots.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016-04-29.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+}
