57fa830b3b4fa3c53c07ff8f1d69b4e88b2fc153
Eric H B Zhan <eric.zhan@163.com>
2016-04-30 08:17:18 +0800

    add thymeleaf message resolver

--- yqboots-webapp/src/main/java/com/yqboots/Application.java
+++ yqboots-webapp/src/main/java/com/yqboots/Application.java
@@ -2,6 +2,9 @@ package com.yqboots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.messageresolver.IMessageResolver;
+import org.thymeleaf.spring4.messageresolver.SpringMessageResolver;
 
 /**
  * Created by Administrator on 2016-04-29.
@@ -10,5 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public IMessageResolver thymeleafMessageResolver() {
+        return new SpringMessageResolver();
     }
 }
