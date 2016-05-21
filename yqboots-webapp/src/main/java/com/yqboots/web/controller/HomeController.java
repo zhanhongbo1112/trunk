d93a9e52a03c927eccd382b9aa0b81d6cbed1107
zhanhongbo1112 <eric.zhan@163.com>
2016-05-21 11:40:51 +0800

    add user profile page

--- yqboots-webapp/src/main/java/com/yqboots/web/controller/HomeController.java
+++ yqboots-webapp/src/main/java/com/yqboots/web/controller/HomeController.java
@@ -12,4 +12,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @RequestMapping(value = "/profile")
+    public String profile() {
+        return "profile/index";
+    }
 }
