f9c1203500321190f15b1e50f87aa6ce93eefea3
zhanhongbo1112 <eric.zhan@163.com>
2016-06-01 06:39:11 +0800

    remove copyright

--- yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/main.js
+++ yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/main.js
@@ -1,6 +1,6 @@
-define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/SearchBox', 'baf/_base/LocaleSwitcher',
-        'baf/_base/StyleSwitcher', 'baf/_base/ScrollToTop', 'jquery/cube-portfolio/cube-portfolio'],
-    function (StickyHeader, MegaMenu, SearchBox, LocaleSwitcher, StyleSwitcher, ScrollToTop) {
+define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/SearchBox', 'baf/_base/StyleSwitcher',
+        'baf/_base/ScrollToTop', 'jquery/cube-portfolio/cube-portfolio'],
+    function (StickyHeader, MegaMenu, SearchBox, StyleSwitcher, ScrollToTop) {
         // We extend jQuery by method hasAttr
         $.fn.hasAttr = function (name) {
             return this.attr(name) !== undefined;
@@ -11,7 +11,6 @@ define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/SearchBox', '
                 StickyHeader.startup();
                 MegaMenu.startup();
                 SearchBox.startup();
-                LocaleSwitcher.startup();
                 StyleSwitcher.startup();
                 ScrollToTop.startup();
             }
