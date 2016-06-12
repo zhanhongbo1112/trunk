9f3c9e90a33a5753262aeef00e22308695ce05f0
zhanhongbo1112 <eric.zhan@163.com>
2016-06-12 16:37:41 +0800

    add error page, upgrade jquery plugin

--- yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/main.js
+++ yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/main.js
@@ -1,18 +1,33 @@
 define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/SearchBox', 'baf/_base/StyleSwitcher',
-        'baf/_base/ScrollToTop', 'jquery/cube-portfolio/cube-portfolio'],
-    function (StickyHeader, MegaMenu, SearchBox, StyleSwitcher, ScrollToTop) {
+        'baf/_base/ScrollToTop', 'baf/_base/dynamicRequire', 'jquery/cube-portfolio/cube-portfolio'],
+    function (StickyHeader, MegaMenu, SearchBox, StyleSwitcher, ScrollToTop, dynReq) {
         // We extend jQuery by method hasAttr
         $.fn.hasAttr = function (name) {
             return this.attr(name) !== undefined;
         };
 
+        // register the pages, based on the template path
+        var pages = {
+            "/prototype/project/index": "baf/prototype/ProjectPage",
+            "/prototype/model/index": "baf/prototype/ModelPage"
+        };
+
         return {
-            startup: function () {
+            startup: function (templateName) {
                 StickyHeader.startup();
                 MegaMenu.startup();
                 SearchBox.startup();
                 StyleSwitcher.startup();
                 ScrollToTop.startup();
+
+                if (!pages[templateName]) {
+                    console.info("No mapping page for template " + templateName);
+                }
+                if (pages[templateName]) {
+                    dynReq.dynamicRequire([pages[templateName]]).then(function (Page) {
+                        Page.startup();
+                    });
+                }
             }
         };
     });
---
+++ yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/prototype/ProjectPage.js
@@ -0,0 +1,7 @@
+define(["jquery/skyforms/maskedinput", "jquery/skyforms/ui", "jquery/skyforms/form"], function () {
+    return {
+        startup: function () {
+            // alert("ProjectPage startup");
+        }
+    };
+});
