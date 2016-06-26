9bbdee9ad40362059481f202f4eb61b0286e770c
zhanhongbo1112 <eric.zhan@163.com>
2016-06-27 06:38:53 +0800

    refactor to different modules for the project initializer and modeler

--- yqboots-prototype/src/main/java/com/yqboots/prototype/model/web/ModelKeys.java
+++
@@ -1,9 +0,0 @@
-package com.yqboots.prototype.model.web;
-
-/**
- * Created by Administrator on 2016-06-09.
- */
-public final class ModelKeys {
-    public static final String BASE_URL = "/prototype/model";
-    public static final String HOME_URL = BASE_URL + "/index";
-}
