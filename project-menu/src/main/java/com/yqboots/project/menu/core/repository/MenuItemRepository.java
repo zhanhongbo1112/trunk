c9169efdeaccfdfb0ea4d5312df770545b466357
zhanhongbo1112 <eric.zhan@163.com>
2016-07-20 06:52:23 +0800

    update data dict module

--- project-menu/src/main/java/com/yqboots/project/menu/core/repository/MenuItemRepository.java
+++ project-menu/src/main/java/com/yqboots/project/menu/core/repository/MenuItemRepository.java
@@ -1,10 +1,12 @@
 package com.yqboots.project.menu.core.repository;
 
+import com.yqboots.project.menu.core.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Created by Administrator on 2016-06-28.
  */
-public interface MenuItemRepository extends JpaRepository {
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long>, JpaSpecificationExecutor<MenuItem> {
 
 }
