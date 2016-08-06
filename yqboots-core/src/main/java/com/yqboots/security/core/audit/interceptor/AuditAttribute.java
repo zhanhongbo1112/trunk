f75a54cf53df7e8580a87961988c6abb1fb2dd33
zhanhongbo1112 <eric.zhan@163.com>
2016-08-06 11:07:22 +0800

    move to another repository

--- yqboots-core/src/main/java/com/yqboots/security/core/audit/interceptor/AuditAttribute.java
+++
@@ -1,8 +0,0 @@
-package com.yqboots.security.core.audit.interceptor;
-
-/**
- * Created by Administrator on 2015-12-14.
- */
-public interface AuditAttribute {
-    int getCode();
-}
--- yqboots-webapp/src/main/resources/META-INF/resources/dojo/baf/_base/MegaMenu.js
+++
@@ -1,9 +0,0 @@
-define([ 'jquery' ], function() {
-    return {
-        startup : function() {
-            $(document).on('click', '.mega-menu .dropdown-menu', function(e) {
-                e.stopPropagation();
-            })
-        }
-    };
-});
