8438e9763fb923da9c2e6e9aae20d2a2444faea6
zhanhongbo1112 <eric.zhan@163.com>
2016-09-21 06:59:48 +0800

    add style switcher

--- project-layout/src/main/resources/META-INF/resources/dojo/baf/main.js
+++ project-layout/src/main/resources/META-INF/resources/dojo/baf/main.js
@@ -1,5 +1,5 @@
-define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/ScrollToTop', 'baf/_base/dynamicRequire'],
-    function (StickyHeader, MegaMenu, ScrollToTop, dynReq) {
+define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/StyleSwitcher', 'baf/_base/ScrollToTop', 'baf/_base/dynamicRequire'],
+    function (StickyHeader, MegaMenu, StyleSwitcher, ScrollToTop, dynReq) {
         // We extend jQuery by method hasAttr
         $.fn.hasAttr = function (name) {
             return this.attr(name) !== undefined;
@@ -9,6 +9,7 @@ define(['baf/_base/StickyHeader', 'baf/_base/MegaMenu', 'baf/_base/ScrollToTop',
             startup: function (templateName) {
                 StickyHeader.startup();
                 MegaMenu.startup();
+                StyleSwitcher.startup();
                 ScrollToTop.startup();
 
                 if (templateName) {
