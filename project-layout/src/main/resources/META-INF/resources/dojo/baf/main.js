bddf34d7cce4f48d63c0f881b2f89ffed09fc837
zhanhongbo1112 <eric.zhan@163.com>
2016-09-27 08:00:12 +0800

    add i18n support for client side

--- project-layout/src/main/resources/META-INF/resources/dojo/baf/main.js
+++ project-layout/src/main/resources/META-INF/resources/dojo/baf/main.js
@@ -5,8 +5,31 @@ define(['baf/_base/dynamicRequire', 'baf/_base/StickyHeader', 'baf/_base/MegaMen
             return this.attr(name) !== undefined;
         };
 
+        // alert($.i18n.prop("hello"));
+        var setLocale = function(locale) {
+            var language = 'en';
+            var country  = '';
+            if(locale) {
+                language = locale['language'];
+                country = locale['country'];
+            }
+
+            if (country) {
+                language = language + '_' + country;
+            }
+
+            $.i18n.properties({
+                name:'messages',
+                path:'/dojo/baf/nls/',
+                mode:'both',
+                language: language,
+                async: true
+            });
+        };
+
         return {
-            startup: function (templateName) {
+            startup: function (templateName, locale) {
+                setLocale(locale);
                 StickyHeader.startup();
                 MegaMenu.startup();
                 StyleSwitcher.startup();
