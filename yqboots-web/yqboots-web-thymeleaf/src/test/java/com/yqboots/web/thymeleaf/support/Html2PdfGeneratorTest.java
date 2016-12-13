9b6c110d98ba6106507a4c497a2ae25411b9c55c
Eric H B Zhan <eric.zhan@163.com>
2016-12-13 20:27:46 +0800

    refactor

--- yqboots-web/yqboots-web-thymeleaf/src/test/java/com/yqboots/web/thymeleaf/support/Html2PdfGeneratorTest.java
+++ yqboots-web/yqboots-web-thymeleaf/src/test/java/com/yqboots/web/thymeleaf/support/Html2PdfGeneratorTest.java
@@ -6,13 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.validation.ObjectError;
 
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {Application.class})
