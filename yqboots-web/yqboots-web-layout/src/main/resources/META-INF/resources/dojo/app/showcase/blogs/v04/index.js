4b5541b9a9b41b246a73ce8780871304ad367050
zhanhongbo1112 <eric.zhan@163.com>
2017-03-19 21:51:21 +0800

    blog

--- yqboots-web/yqboots-web-layout/src/main/resources/META-INF/resources/dojo/app/showcase/blogs/v04/index.js
+++ yqboots-web/yqboots-web-layout/src/main/resources/META-INF/resources/dojo/app/showcase/blogs/v04/index.js
@@ -1,7 +1,19 @@
-define([], function () {
+define(['jquery/fancybox/fancybox'], function () {
     return {
         startup: function () {
-
+            $(".fancybox").fancybox({
+                groupAttr: 'data-rel',
+                prevEffect: 'fade',
+                nextEffect: 'fade',
+                openEffect: 'elastic',
+                closeEffect: 'fade',
+                closeBtn: true,
+                helpers: {
+                    title: {
+                        type: 'float'
+                    }
+                }
+            });
         }
     }
 });
--- yqboots-web/yqboots-web-layout/src/main/resources/META-INF/resources/dojo/app/showcase/blogs/v08/index.js
+++ yqboots-web/yqboots-web-layout/src/main/resources/META-INF/resources/dojo/app/showcase/blogs/v08/index.js
@@ -1,7 +1,6 @@
 define([], function () {
     return {
         startup: function () {
-
         }
     }
 });
